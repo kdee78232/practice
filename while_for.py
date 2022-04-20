5c3ca4620a5ae180488cd0200eab92236c300592
Kevin <https://github.com/kdee78232/practice.git>
2022-04-20 10:46:30 -0400

    New file the start

---
+++ while_for.py
@@ -0,0 +1,39 @@
+food=('fish','steak','meatloaf','tuna')
+for variable in food:
+    print(variable)
+
+
+number=344
+val=input('Enter your number:\n ')
+if int(val) == 344:
+    print('you are correct')
+elif int(val)<= 344:
+    print('You are wrong')
+else:
+    print('try again')
+
+
+for x in range(1,8):
+    print(x)
+
+
+for x in range(1,45,8):
+    print(x)
+
+
+
+while True:
+    string=input('Enter your work status.\nWhat is your status  ')
+    if string=='new hire':
+        break
+    print('try again')
+print(f'Welcome Aboard')
+
+
+
+attempts=1
+while (attempts < 20):
+    print('tries count is> ',attempts)
+    attempts=attempts+1
+else:
+    print('you are done')
